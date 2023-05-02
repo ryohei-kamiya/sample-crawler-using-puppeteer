6c653578de46994d7b4a31b3e1a73d8b86d750ad
ryohei-kamiya <contact@ww-auto.net>
2023-05-02 16:09:47 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,117 @@
+import argparse
+import re
+import asyncio
+import traceback
+from urllib.parse import urlparse
+from pyppeteer import launch
+
+
+async def crawl_pages(
+    urls: set[str],
+    output_dir: str,
+    target_domains: set = set(),
+    processed_urls: set = set(),
+    excluded_urls: set = set(),
+    depth: int = -1,
+):
+    if not bool(urls):  # クロール対象URLリスト(urls)が空の場合は処理を中断
+        return
+    urls_buf = set()
+    for url in urls:
+        if url in excluded_urls:  # excluded_urls に含まれるURLはクロール対象から除外
+            continue
+        try:
+            page = await browser.newPage()
+            await page.goto(url, waitUntil="networkidle0")  # ページ読み込みが完了するまで待機
+
+            # ページ内のリンクを取得
+            sub_links = await page.evaluate(
+                """() => {
+                return Array.from(document.querySelectorAll('a'))
+                    .map(link => link.href);
+            }"""
+            )
+
+            # ページ内のコンテンツを取得
+            content = await page.content()
+
+            await page.close()
+
+            # クローリングしたデータを出力
+            output_filename = re.sub(r"[^\-0-9a-zA-Z\.]", "_", url)
+            print(output_filename)
+            output_filepath = "/".join([output_dir, output_filename])
+            with open(output_filepath, "w") as fout:
+                fout.write(content)
+
+            # ページ内のリンクリストを出力
+            output_filename = re.sub(r"[^\-0-9a-zA-Z\.]", "_", url)
+            output_filename = f"urls_in_{output_filename}"
+            print(output_filename)
+            output_filepath = "/".join([output_dir, output_filename])
+            with open(output_filepath, "w") as fout:
+                for sub_link in sub_links:
+                    sub_link = sub_link.strip()
+                    if not sub_link:  # 空文字は出力しない
+                        continue
+                    fout.write(f"{sub_link}\n")
+
+            processed_urls.add(url)  # クロール済みURLは、処理済みURLリストに追加
+            urls_buf = urls_buf | set(sub_links)
+        except Exception:
+            traceback.print_exc()
+
+    # クロール済みリンク、外部リンクは、次のクロール対象(next_urls)から除去する
+    next_urls = set()
+    links = urls_buf - processed_urls - excluded_urls
+    for link in links:
+        link_parsed_url = urlparse(link)
+        link_domain = link_parsed_url.hostname
+        if link_domain in target_domains:  # クロール対象ドメイン配下のリンクのみnext_urlsに追加
+            next_urls.add(link)
+        else:  # 外部リンクは excluded_urlsに追加
+            excluded_urls.add(link)
+
+    if depth > 0 or depth < 0:  # 探索の深さが 0 以外の場合に、次の深さのクロールを実行
+        await crawl_pages(
+            next_urls,
+            output_dir,
+            target_domains,
+            processed_urls,
+            excluded_urls,
+            depth - 1,
+        )
+
+
+async def main(args):
+    global browser
+    output_dir = "./output"
+
+    urls = set()
+    target_domains = set()
+    with open(args.urllistfile, "r") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            url = line.strip()
+            if url.startswith("#"):
+                continue
+            parsed_url = urlparse(url)
+            target_domain = parsed_url.hostname
+            urls.add(url)
+            target_domains.add(target_domain)
+
+    browser = await launch(headless=True)
+    await crawl_pages(urls, output_dir, target_domains, set(), set(), depth=args.depth)
+    await browser.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A sample crawler using Puppeteer")
+    parser.add_argument("urllistfile", type=str, help="URL list for crawling")
+    parser.add_argument(
+        "--depth", type=int, default=-1, help="Set the maximum number of sublinks"
+    )
+    args = parser.parse_args()
+    asyncio.get_event_loop().run_until_complete(main(args))
