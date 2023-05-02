cc4f42d27da8126c9cbc205d967ca8683edd9af3
ryohei-kamiya <contact@ww-auto.net>
2023-05-02 19:06:36 +0900

    feat: add redirect detection

--- main.py
+++ main.py
@@ -12,58 +12,82 @@ async def crawl_pages(
     target_domains: set = set(),
     processed_urls: set = set(),
     excluded_urls: set = set(),
+    redirected_urls: dict = {},
     depth: int = -1,
+    limit: int = 5,  # 同時にクロールするURLは5つまで
 ):
     if not bool(urls):  # クロール対象URLリスト(urls)が空の場合は処理を中断
         return
-    urls_buf = set()
-    for url in urls:
+
+    sem = asyncio.Semaphore(limit)
+
+    async def crawl_page(url: str, output_dir: str, sub_urls: set = set()):
         if url in excluded_urls:  # excluded_urls に含まれるURLはクロール対象から除外
-            continue
-        try:
-            page = await browser.newPage()
-            await page.goto(url, waitUntil="networkidle0")  # ページ読み込みが完了するまで待機
+            return
 
-            # ページ内のリンクを取得
-            sub_links = await page.evaluate(
-                """() => {
-                return Array.from(document.querySelectorAll('a'))
-                    .map(link => link.href);
-            }"""
-            )
+        async def redirect_handler(response):
+            status = response.status
+            if 300 <= status <= 399:
+                redirected_urls[response.url] = response.headers["location"]
+                print(
+                    f"[Detected redirect]{response.url} -> {response.headers['location']}"
+                )
 
-            # ページ内のコンテンツを取得
-            content = await page.content()
+        async with sem:
+            try:
+                browser = await launch(headless=True)
+                page = await browser.newPage()
+                page.on(
+                    "response",
+                    lambda response: asyncio.ensure_future(redirect_handler(response)),
+                )
+                await page.goto(url, waitUntil="networkidle0")  # ページ読み込みが完了するまで待機
 
-            await page.close()
+                # ページ内のコンテンツを取得
+                content = await page.content()
 
-            # クローリングしたデータを出力
-            output_filename = re.sub(r"[^\-0-9a-zA-Z\.]", "_", url)
-            print(output_filename)
-            output_filepath = "/".join([output_dir, output_filename])
-            with open(output_filepath, "w") as fout:
-                fout.write(content)
-
-            # ページ内のリンクリストを出力
-            output_filename = re.sub(r"[^\-0-9a-zA-Z\.]", "_", url)
-            output_filename = f"urls_in_{output_filename}"
-            print(output_filename)
-            output_filepath = "/".join([output_dir, output_filename])
-            with open(output_filepath, "w") as fout:
+                # ページ内のリンクリストを取得
+                sub_links = await page.evaluate(
+                    """() => {
+                    return Array.from(document.querySelectorAll('a'))
+                        .map(link => link.href);
+                }"""
+                )
                 for sub_link in sub_links:
-                    sub_link = sub_link.strip()
-                    if not sub_link:  # 空文字は出力しない
-                        continue
-                    fout.write(f"{sub_link}\n")
+                    sub_urls.add(sub_link)
 
-            processed_urls.add(url)  # クロール済みURLは、処理済みURLリストに追加
-            urls_buf = urls_buf | set(sub_links)
-        except Exception:
-            traceback.print_exc()
+                await page.close()
+                await browser.close()
+
+                # クローリングしたデータを出力
+                output_filename = re.sub(r"[^\-0-9a-zA-Z\.]", "_", url)
+                print(output_filename)
+                output_filepath = "/".join([output_dir, output_filename])
+                with open(output_filepath, "w") as fout:
+                    fout.write(content)
+
+                # ページ内のリンクリストを出力
+                output_filename = re.sub(r"[^\-0-9a-zA-Z\.]", "_", url)
+                output_filename = f"urls_in_{output_filename}"
+                print(output_filename)
+                output_filepath = "/".join([output_dir, output_filename])
+                with open(output_filepath, "w") as fout:
+                    for sub_link in sub_links:
+                        sub_link = sub_link.strip()
+                        if not sub_link:  # 空文字は出力しない
+                            continue
+                        fout.write(f"{sub_link}\n")
+                processed_urls.add(url)  # クロール済みURLは、処理済みURLリストに追加
+            except Exception:
+                traceback.print_exc()
+
+    sub_urls = set()
+    tasks = [crawl_page(url, output_dir, sub_urls) for url in urls]
+    await asyncio.gather(*tasks)
 
     # クロール済みリンク、外部リンクは、次のクロール対象(next_urls)から除去する
     next_urls = set()
-    links = urls_buf - processed_urls - excluded_urls
+    links = (urls | sub_urls) - processed_urls - excluded_urls
     for link in links:
         link_parsed_url = urlparse(link)
         link_domain = link_parsed_url.hostname
@@ -79,6 +103,7 @@ async def crawl_pages(
             target_domains,
             processed_urls,
             excluded_urls,
+            redirected_urls,
             depth - 1,
         )
 
@@ -102,9 +127,53 @@ async def main(args):
             urls.add(url)
             target_domains.add(target_domain)
 
-    browser = await launch(headless=True)
-    await crawl_pages(urls, output_dir, target_domains, set(), set(), depth=args.depth)
-    await browser.close()
+    processed_urls = set()
+    excluded_urls = set()
+    redirected_urls = {}
+    await crawl_pages(
+        urls,
+        output_dir,
+        target_domains,
+        processed_urls,
+        excluded_urls,
+        redirected_urls,
+        depth=args.depth,
+        limit=5,
+    )
+
+    # 処理済みURLのリストを出力
+    output_filename = "all_processed_urls.txt"
+    print(output_filename)
+    output_filepath = "/".join([output_dir, output_filename])
+    with open(output_filepath, "w") as fout:
+        for url in processed_urls:
+            url = url.strip()
+            if not url:  # 空文字は出力しない
+                continue
+            fout.write(f"{url}\n")
+
+    # 処理対象外URLのリストを出力
+    output_filename = "all_excluded_urls.txt"
+    print(output_filename)
+    output_filepath = "/".join([output_dir, output_filename])
+    with open(output_filepath, "w") as fout:
+        for url in excluded_urls:
+            url = url.strip()
+            if not url:  # 空文字は出力しない
+                continue
+            fout.write(f"{url}\n")
+
+    # リダイレクトURLのリストを出力
+    output_filename = "all_redirected_urls.txt"
+    print(output_filename)
+    output_filepath = "/".join([output_dir, output_filename])
+    with open(output_filepath, "w") as fout:
+        for url, location in redirected_urls.items():
+            url = url.strip()
+            if not url:  # 空文字は出力しない
+                continue
+            location = location.strip()
+            fout.write(f"{url} => {location}\n")
 
 
 if __name__ == "__main__":
